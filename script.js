c6d013221399cbc0e68371257c788e9cd4f906cb
abbas alme <abbasalme99@gmail.com>
2023-06-28 16:59:58 -0700

    update index and js files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let firstName = "Abbas";
-let lastName = "Alme";
-let yearsOfStudy = 1; // Replace 2 with the number of years you have studied at SCC
-let goal = "pursue a career in software development";
+var firstName = "Abbas";
+var lastName = "Alme";
+var yearsOfStudy = 1; // Replace 2 with the number of years you have studied at SCC
+var goal = "pursue a career in software development";
