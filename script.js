5a40fa45e112e7d47b74c58638666f196a045b81
abbas alme <abbasalme99@gmail.com>
2023-06-28 16:35:38 -0700

    create files

---
+++ script.js
@@ -0,0 +1,4 @@
+let firstName = "Abbas";
+let lastName = "Alme";
+let yearsOfStudy = 1; // Replace 2 with the number of years you have studied at SCC
+let goal = "pursue a career in software development";
